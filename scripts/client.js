b0374e7e35bc0f3e1c1fe3ca1850f28f4a9bc367
alex braatz <alexjbraatz@gmail.com>
2021-03-05 16:32:10 -0600

    can add employee info to object, push object to employee array and return array with employee info to console

--- scripts/client.js
+++ scripts/client.js
@@ -1,9 +1,24 @@
 $( document ).ready( readyNow );
 
+let allEmployees = [];
+
 function readyNow(){
     $( '#submitButton' ).on( 'click', addEmployee ); 
 }
 
 function addEmployee(){
     console.log( 'in addEmployee!' );
+
+    let newEmployee = {
+        fname: $( '#firstNameIn' ).val( ),
+        lname: $( '#lastNameIn' ).val( ),
+        id: $( '#idIn' ).val( ),
+        title: $( '#titleIn' ).val( ),
+        salary: $( '#salaryIn' ).val( )
+    }
+
+    $( '.inputText' ).val( '' );
+
+    allEmployees.push( newEmployee );
+    return allEmployees;
 }
